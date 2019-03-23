684dedbec2d6474135c4efd9659fb7d5532ea52e
Mahdi Kariman <mp.kariman@gmail.com>
2019-03-23 06:54:30 -0700

    full part1

--- HW1/HW1/Program.cs
+++ HW1/HW1/Program.cs
@@ -11,10 +11,16 @@ namespace HW1
         static void Main(string[] args)
         {
             int[] x = new int[10];
+            int max = 0;
             for (int n = 0; n < x.Length; n++)
-                   x[n] = Convert.ToInt16(Console.ReadLine());
-            foreach (int n in x)
-                Console.WriteLine(x[n-1]);
+                x[n] = Convert.ToInt16(Console.ReadLine());
+            max = x[9];
+            for(int i=8;i>=0;i--)
+            if (x[i] > max)
+                max = x[i];
+            Console.WriteLine("in meghdare maximum hastesh : {0}",max);
+
+
             Console.ReadKey();
 
         }
