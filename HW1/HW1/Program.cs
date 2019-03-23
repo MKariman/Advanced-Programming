e58c984793ab2d9b7b0024f024e98f789ffd2741
Mahdi Kariman <mp.kariman@gmail.com>
2019-03-23 06:40:55 -0700

    part1
    hw1-part1

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[10];
+            for (int n = 0; n < x.Length; n++)
+                   x[n] = Convert.ToInt16(Console.ReadLine());
+            foreach (int n in x)
+                Console.WriteLine(x[n-1]);
+            Console.ReadKey();
+
+        }
+    }
+}
