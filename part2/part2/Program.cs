c0eab50806657c9e041ca9948bb9fee132865c6b
Mahdi Kariman <mp.kariman@gmail.com>
2019-03-24 03:28:35 -0700

    Part2
    part2 hw1 is ready!

---
+++ part2/part2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myDate = DateTime.Now;
+            Console.WriteLine("{0} or {1}",myDate.ToLongDateString(),myDate.ToShortDateString());
+            Console.ReadKey();
+        }
+    }
+}
